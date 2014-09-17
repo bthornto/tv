6592c505b77cfcc1be44e6965a2890f946a2e47e
Bradley A. Thornton <bthornto@starbucks.com>
2014-09-17 14:23:01 -0700

    Initial

---
+++ app/controllers/watch_controller.rb
@@ -0,0 +1,46 @@
+class WatchController < ApplicationController
+  
+
+  require 'pp'
+  include Sys
+
+  def curl(url)
+		c = Curl::Easy.new(url) do |curl|
+			curl.http_auth_types = :basic
+			#curl.userpwd = "admin:#{Settings.solarwinds_admin_password}"
+			#curl.username = 'admin'
+			#curl.password = Settings.solarwinds_admin_password
+			curl.ssl_verify_host = 0
+			curl.ssl_verify_peer = false
+			curl.verbose = false
+			curl.use_ssl = 3
+			curl.ssl_version = 3
+			curl.headers['Accept'] = 'application/json'
+		end
+		c.perform
+		response = JSON.parse(c.body_str)
+		return response
+	end
+
+  def index
+  	@stations = curl("http://192.168.1.165/lineup.json")
+  end
+
+  	def wait
+  		stuff = ProcTable.ps
+		stuff.each do |process|
+			if process.cmdline.to_s.include? "ffmpeg"
+				puts process.pid
+				puts process.cmdline.to_s
+				Process.kill("HUP", process.pid)
+			end
+		end
+  		pid = fork do
+  			exec "ffmpeg -i http://192.168.1.165:5004/auto/v#{params[:station]}?transcode=internet480 -async 1 -ss 00:00:05 -acodec aac -strict -2  -b:v 3000k -ac 2 -vcodec copy -preset superfast  -tune zerolatency  -threads 2 -s 1280x720 -flags -global_header -fflags +genpts -map 0:0 -map 0:1 -hls_time 2 -hls_wrap 40 public/test.m3u8 > /dev/null"
+		end
+	end
+
+	def watch
+	
+	end
+end
